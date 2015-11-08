bc7ae30213f11f1b9429f33194df28aa7a4b1f7d
Stephen Lazaro <stephen.lazaro@gmail.com>
2015-11-08 11:34:48 -0800

    These.

--- plot1.r
+++ plot1.r
@@ -1,7 +1,14 @@
 #load data (use colClasses?) It's the time field that doesn't make sense
+#As written, must be run in same directory as data table.
 x <- read.table("household_power_consumption.txt", sep=";", 
                 na.strings= "?", header=TRUE, nrows=2075259,
                 colClasses = c("character", "character", "numeric", "numeric", "numeric", 
                                "numeric", "numeric", "numeric", "numeric"))
-x$Time = strptime(x$Time, "%H:%M:%S") #this adds the date data for some reason
-x$Date = as.Date(x$Date, "$d/%m/%Y")
+x$Time = strptime(x$Time, "%H:%M:%S")
+x$Date = as.Date(x$Date, "%d/%m/%Y")
+# Hardcoded because lazy, had difficulties with 
+# more flexible subsetting for some reason.
+x <- x[66637:69516,]
+hist(x$Global_active_power, col="red", 
+     xlab="Global Active Power (kilowatts)", 
+     main="Global Active Power")
